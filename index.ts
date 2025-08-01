bbff79d2ed4019a62099d72f0a073b99c897ea1c
hargrove <hargrove.infinity@gmail.com>
2025-08-01 13:14:55 +0300

    added catch all route handler

--- index.ts
+++ index.ts
@@ -10,6 +10,11 @@ app.use(jsonMiddleware);
 // API router
 app.use(BaseRouter);
 
+// Catch all routes, not found route error
+app.all("/{*splat}", (req, res) => {
+  res.status(404).send("Route not found");
+});
+
 /* RUN APP */
 app.listen(ENV.Port, () => {
   console.log(`Server is running on ${ENV.Port}`);
