908682544cd2a868fafe020bd77f62719996b327
hargrove <hargrove.infinity@gmail.com>
2025-08-12 20:06:29 +0300

    removed unused types from validation schema

--- validation/types.ts
+++ validation/types.ts
@@ -1,23 +1,9 @@
 import { z } from "zod";
-import {
-  categoryDefSchema,
-  categoryUpdateSchema,
-  materialDefSchema,
-  materialUpdateSchema,
-  recommendedMaterialDefSchema,
-} from "./schemas";
+import { recommendedMaterialDefSchema } from "./schemas";
 
 /* TYPES FROM VALIDATION SCHEMAS */
 
-// Categories
-export type CategoryDef = z.infer<typeof categoryDefSchema>;
-
-export type CategoryUpdate = z.infer<typeof categoryUpdateSchema>;
-
 // Materials
-export type MaterialDef = z.infer<typeof materialDefSchema>;
-
-export type MaterialUpdate = z.infer<typeof materialUpdateSchema>;
 
 export type RecommendedMaterialDef = z.infer<
   typeof recommendedMaterialDefSchema
