cc24d143f4f5095f653cdc6118efa7c3dbb6d9b5
hargrove <hargrove.infinity@gmail.com>
2025-08-23 13:37:14 +0300

    populated getAllMenteesWithMaterials endpoint

--- routes/reportRoutes.ts
+++ routes/reportRoutes.ts
@@ -1,8 +1,24 @@
 import { Request, Response } from "express";
+import { eq } from "drizzle-orm";
+import { db, materialTable, menteeTable } from "../drizzle";
 
-async function getAllMenteesWithMaterials(req: Request, res: Response) {
+async function getAllMenteesWithMaterials(
+  req: Request,
+  res: Response
+): Promise<void> {
   try {
-    res.send("OK");
+    const result = await db
+      .select({
+        menteeId: menteeTable.id,
+        firstName: menteeTable.firstName,
+        lastName: menteeTable.lastName,
+        url: materialTable.url,
+        type: materialTable.type,
+      })
+      .from(menteeTable)
+      .innerJoin(materialTable, eq(menteeTable.id, materialTable.menteeId));
+
+    res.send(result);
   } catch (error) {
     console.error(error);
     res.status(500).send("Error in get all mentees with materials");
